83328ebe7c17ccd80593206ae8bcc4d754b9322f
NenaLotte <n.l.v.as@student.tue.nl>
2018-06-05 10:40:28 +0200

    Functie om geselecteerde waarde te returnen uit de dilemma's.

--- js/controller/dilemma1Controller.js
+++ js/controller/dilemma1Controller.js
@@ -1,6 +1,12 @@
 var Dilemma1Controller = function ( model, view ){
 
   view.volgendeButton.click(function(){
+
+        //Look into table with class 'case' for fields with 'input', 'radio', AND 'checked' and return for each of those...
+        $("table.case input:radio:checked").each(function () {
+          console.log(this.value); // A console log with the value.
+        });
+
         model.dilemma1Done();
         //model.setDilemma1();
   });
