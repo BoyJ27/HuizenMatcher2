c8d7197711fc8c65cd459aca6264d27e7d7eeef2
NenaLotte <nenavanas@gmail.com>
2018-06-08 13:25:35 +0200

    Opschonen instructionscontroller

--- js/controller/instructionsController.js
+++ js/controller/instructionsController.js
@@ -1,15 +1,5 @@
 var InstructionsController = function ( model, view ){
 
-    /*********************************
-    TESTING
-    *********************************/
-    // view.testButton.click(function(){
-    //   model.shuffleHouses(model.houses);
-    // });
-    /*********************************
-    END OF TESTING
-    *********************************/
-
     view.volgendeButton.click(function(){
           model.instructionsDone();
         });
